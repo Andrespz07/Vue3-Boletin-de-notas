884bd9df730941621782f894e4a3bfd7c0eee79f
Andrespz07 <andresperezortega2001@gmail.com>
2023-03-20 16:50:39 +0100

    clean repository

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+const props = headerProps({
+  title:{
+    type: String,
+    required: true,
+  }
+});
+</script>
+<template>
+  <header class="header">
+    <h1>{{props.title}}</h1>
+  </header>
+</template>
+<style lang="scss" scoped>
+
+header{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+}
+</style>
